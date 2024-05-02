c9122b7d9b05bf709be330d6acdd8346064a06cc
manudevelopia <manugarciarodriguez@gmail.com>
2024-05-02 11:21:35 +0200

    add html report

--- library/src/main/java/info/developia/testing/poc/TestRunner.java
+++ library/src/main/java/info/developia/testing/poc/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "classpath:features",
-        plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json"},
+        plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json", "html:target/cucumber-reports/report.html" },
         glue = "info.developia.testing.poc.steps")
 public class TestRunner {
 
